2bd1c06daf00b30d64cd7f866751ae38cdc6ab77
ganiefah <ganiefahmajiet3@gmail.com>
2023-07-01 14:39:08 +0200

    navbar

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,18 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div>
+    <Navbar/>
+    <router-view/>
+  </div>
 </template>
+
+<script>
+    import Navbar from '@/components/NavBar'
+   export default {
+   components: {
+   Navbar,
+ }
+} 
+</script>
 
 <style>
 #app {
@@ -15,16 +23,4 @@
   color: #2c3e50;
 }
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
